bb403fc37e5d14672b70b5c0f3dcfffb23d4b5e9
Fernando Yang <hagisf@usp.br>
2024-09-13 19:05:56 -0300

    Improve UI number display

--- metroid-like/UI/GameSceneUI.gd
+++ metroid-like/UI/GameSceneUI.gd
@@ -2,10 +2,10 @@ extends Node
 
 func _ready() -> void:
 	GlobalSignals.missile_ui_update.connect(_missile_ui_update)
-	$Missile/MissileQty.text = str(0)
+	$Missile/MissileQty.text = str(0).pad_zeros(3)
 
 func _process(delta: float) -> void:
 	pass
 
 func _missile_ui_update(value) -> void:
-	$Missile/MissileQty.text = str(value)
+	$Missile/MissileQty.text = str(value).pad_zeros(3)
