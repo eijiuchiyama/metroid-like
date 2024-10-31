69aeabf8af1dbedfc952873e8759d81440be7f4d
Fernando Yang <hagisf@usp.br>
2024-10-31 19:38:48 -0300

    Refatorar player.gd, corrigir bug is_human()

--- metroid-like/Characters/Player/player.gd
+++ metroid-like/Characters/Player/player.gd
@@ -3,34 +3,27 @@ class_name Player extends CharacterBody2D
 @onready var animation_manager = get_node("AnimationManager")
 @onready var player_arm = get_node("PlayerArm")
 
-var horizontal_speed : int
-var vertical_speed : int
-var gravity: int
 enum mode {
 	Human,
-	Ball	
+	Ball,
 } 
-var current_mode : mode
-var previous_direction : int
 
-func _ready() -> void:
-	previous_direction = 1
-	current_mode = mode.Human
-	horizontal_speed = 500
-	vertical_speed = 350
-	gravity = 980
+var horizontal_speed : int = 500
+var jump_force : int = -350
+var gravity: int = 980
+var current_mode : mode = mode.Human
+var previous_direction : int = 1
 	
 func _process(delta: float) -> void:
-	var input_direction = Input.get_vector("left", "right", "jump", "down")
+	var input_direction = Input.get_vector("left", "right", "up", "down")
 	self.velocity.x = input_direction.x * horizontal_speed
 	if self.velocity.x != 0:
 		previous_direction = sign(self.velocity.x)
-	if not is_on_floor():
-		self.velocity.y += gravity * delta
-	else:
-		self.velocity.y = 0
+	
+	self.velocity.y = self.velocity.y + gravity * delta if not is_on_floor() else 0
+	
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		self.velocity.y -= vertical_speed
+		self.velocity.y = jump_force
 		
 	player_arm.scale.x = previous_direction
 	if previous_direction == -1:
