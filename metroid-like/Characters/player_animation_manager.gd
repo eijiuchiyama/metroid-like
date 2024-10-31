a38e39a4650526188c667884cb8964fe1e1d4648
Fernando Yang <hagisf@usp.br>
2024-10-31 19:14:00 -0300

    Prever o possivel erro do direcionamento do sprite da personagem apos mudar de cena

--- metroid-like/Characters/player_animation_manager.gd
+++ metroid-like/Characters/player_animation_manager.gd
@@ -3,24 +3,26 @@ class_name AnimationManager extends Node
 @export var ball_sprite : AnimatedSprite2D
 @export var human_sprite : AnimatedSprite2D
 
-@export var current_sprite : AnimatedSprite2D
+var current_sprite : AnimatedSprite2D
 
-func _ready():
+func _enter_tree():
+	current_sprite = human_sprite
 	current_sprite.show()
 	
 func toggle_sprite(mode : String):
 	if mode == "ball":
+		ball_sprite.flip_h = current_sprite.flip_h
 		current_sprite = ball_sprite
 		ball_sprite.show()
 		human_sprite.hide()
 	elif mode == "human":
+		human_sprite.flip_h = current_sprite.flip_h
 		current_sprite = human_sprite
 		ball_sprite.hide()
 		human_sprite.show()
 
 func toggle_animation(anim : String):
-	human_sprite.play(anim)
-	ball_sprite.play(anim)
+	current_sprite.play(anim)
 
 func flip_animation(flip_val : bool):
 	current_sprite.flip_h = flip_val
