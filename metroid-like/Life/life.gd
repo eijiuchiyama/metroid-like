706bcbdd4a036fc9345e270c19c3c303465789aa
atenagoras <atenagoras@zumbidospalmares>
2024-11-20 22:50:46 -0300

    Mudanças na classe Life e criação da classe cureFactor, que herda a classe Life: - Falta meio de adicionar pontos de vida quando o item de pegar vida é capturado - Falta preencher função _process nas duas classes - Quando o sprite da personagem toca em algum inimigo ou toma tiros, falta fazer com que os pontos de vida diminuam utilizando o metodo subHp(), que já está pronto - Falta criar a classe Vampire - Implementar padrão Estratégia (ou algum outro) para personagem adquirir fator cura, ou já inicializá-la com a classe cureFactor?

--- metroid-like/Life/life.gd
+++ metroid-like/Life/life.gd
@@ -1,27 +1,33 @@
-class_name Life extends Node2D
-
+class_name Life extends Node
 var hp : int
 var hpMax : int
 var is_visible : bool # visible é variavel reservado, não se pode usar
 
-func _init(_hp, _hpMax, _visible): #não pode colocar var nos parametros, para definir tipo, use XXX : YYY
-	setHp(_hp)
-	setHpMax(_hpMax)
+func _init(healthPoints : int, maxHealthPoints :int, Visible : bool): #não pode colocar var nos parametros, para definir tipo, use XXX : YYY
+	setHp(healthPoints)
+	setHpMax(maxHealthPoints)
 
 func getHp() -> int:
 	return hp
 func getHpMax() -> int:
 	return hpMax
-func setHp(_hp) -> void:
-	hp = _hp
+func setHp(healthPoints) -> void:
+	hp = healthPoints
 	return
-func setHpMax(_hpMax) -> void:
-	hpMax = _hpMax
+func setHpMax(maxHealthPoints) -> void:
+	hpMax = maxHealthPoints
 	return
 func addHp(n) -> void:
-	hp = hp + n
+	if not hp + n < hpMax:
+		hp = hp + n
+	else:
+		hp = hpMax
 	return
 func subHp(n) -> void:
-	addHp(-n)
+	if not hp - n < 0:
+		addHp(-n)
+	return
+	hp = 0
+	return
 func special() -> void: # Seria uma função virtual em C++ a ser implementada na subclasse que herdar Life (associada ao tipo de personagem que tem a vida
-	return # Se a subclasse não tiver um polimorfismo para esta função, a função apenas não faz nada
+	pass # Se a subclasse não tiver um polimorfismo para esta função, a função apenas não faz nada
