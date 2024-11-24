f49b439682d16fbed7d6a9800151f3781b9e45c9
Marcelo Mendes Spessoto Junior <marcelomspessoto@gmail.com>
2024-11-24 18:04:20 -0300

    Fix ball to human transition
    Signed-off-by: Marcelo Mendes Spessoto Junior <marcelomspessoto@gmail.com>

--- metroid-like/Characters/Player/ModeStates/ball_state.gd
+++ metroid-like/Characters/Player/ModeStates/ball_state.gd
@@ -3,6 +3,7 @@ extends State
 @export var human_state: State
 @onready var collision_human = get_parent().get_parent().get_node("CollisionShapeHuman")
 @onready var collision_ball = get_parent().get_parent().get_node("CollisionShapeBall")
+@onready var player_hitbox = get_parent().get_parent().get_node("PlayerHitbox")
 
 func init_state() -> void:
 	player_arm.hide()
@@ -11,5 +12,7 @@ func init_state() -> void:
 	collision_ball.disabled = false
 	
 func update_state() -> void:
-	if Input.is_action_just_pressed("up"):
-		state_manager.switch_state(human_state)
+	if Input.is_action_just_pressed("up") and not (Input.is_action_pressed("left") or Input.is_action_pressed("right")):
+		var overlapping_bodies = player_hitbox.get_overlapping_bodies()
+		if not len(overlapping_bodies) > 1:
+			state_manager.switch_state(human_state)
