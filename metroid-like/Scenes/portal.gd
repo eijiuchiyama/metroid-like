4bfda8f5d454c7f3c526a92b867919564ad777b3
eijiuchiyama <eijiuchiyama@usp.br>
2024-12-01 01:26:03 -0300

    Portal funcionando

---
+++ metroid-like/Scenes/portal.gd
@@ -0,0 +1,23 @@
+extends StaticBody2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$AnimatedSprite2D.play("default")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_area_2d_2_body_entered(body: Node2D) -> void:
+	if("player" in body.get_groups()):
+		$AnimatedSprite2D.play("opening")
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if($AnimatedSprite2D.animation == "opening"):
+		$CollisionShape2D.disabled = true
+		$Area2D2.queue_free()
+		$AnimatedSprite2D.play("opened")
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if("player" in body.get_groups()):
+		get_tree().change_scene_to_file("res://Scenes/boss_room.tscn")
